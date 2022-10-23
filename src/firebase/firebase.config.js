df5ac12617eee8696ec5d65e98462420585b456f
hasibferdous <hasibferdous.ict@gmail.com>
2022-10-23 23:54:13 +0600

    first commit

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAX-xU4rKpv6W4ehCBen5z_xJ9X1UaVIxQ",
+  authDomain: "exploring-bangladesh-ec8c0.firebaseapp.com",
+  projectId: "exploring-bangladesh-ec8c0",
+  storageBucket: "exploring-bangladesh-ec8c0.appspot.com",
+  messagingSenderId: "546477802818",
+  appId: "1:546477802818:web:e61d099aee386c666674db"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
